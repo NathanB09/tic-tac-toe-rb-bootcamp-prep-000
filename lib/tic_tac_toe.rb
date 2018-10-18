b791dc3402bab0e080a93bfbf0bb6ca8fa67556c
Nathan Bell <nathanbell09@gmail.com>
2018-10-18 11:24:41 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,10 +47,8 @@ end
 
 def turn_count(board)
   turn = 0
-  if !over?(board)
-    board.each do |i|
-      i == "X" || i == "O" ? turn += 1 : nil
-    end
+  board.each do |i|
+    i == "X" || i == "O" ? turn += 1 : nil
   end
   turn
 end
